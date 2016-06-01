18c68bda967af00de61dd1487e564540dcffa3a7
Aaron <newcomer.aaron@icloud.com>
2016-05-31 22:14:13 -0400

    add styling

--- dynamicbox/dynamicbox.js
+++ dynamicbox/dynamicbox.js
@@ -1,12 +1,23 @@
-$(".morebox").each(function(i){
-  $(".morebox").change(function(){
+
+var morebox = $(".morebox");
+
+$.each(morebox, function(i, morebox){
+  $(morebox).change(function(){
     $(".wrapper").append("<input class =morebox type=text/>");
     console.log("Hello");
 
-  })
+  });
+
+  //  $(".morebox").blur(function(){
+  //    var morebox = $(".morebox");
+   //
+  //    if (morebox.HTML=""){
+  //         morebox.remove();
+  //    }
+  //  });
   // $(".wrapper").append("<input class =morebox type=text/>");
   // console.log("Hello");
-});
+ });
 
 
 // $(".morebox").change(function(){
